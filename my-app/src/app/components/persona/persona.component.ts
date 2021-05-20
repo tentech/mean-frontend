b0aa90440c739a49feb68629061c0a047abc049c
Bryan Erazo <bryan07steven@gmail.com>
2021-05-20 16:07:14 -0500

    Ber-v2

--- my-app/src/app/components/persona/persona.component.ts
+++ my-app/src/app/components/persona/persona.component.ts
@@ -2,43 +2,74 @@ import { Component, OnInit } from '@angular/core';
 
 import { NgForm } from "@angular/forms";
 import { PersonaService } from "../../services/persona.service";
+import { VacunaService } from "../../services/vacuna.service";
+import { CiudadService } from "../../services/ciudad.service";
+import { ProvinciaService } from "../../services/provincia.service";
 import {Persona} from "../../models/persona"
-
+import { Provincia } from 'src/app/models/provincia';
 
 @Component({
   selector: 'app-persona',
   templateUrl: './persona.component.html',
   styleUrls: ['./persona.component.css'],
-  providers: [PersonaService],
+  providers: [PersonaService, 
+              VacunaService, 
+              CiudadService,
+              ProvinciaService]
 })
 export class PersonaComponent implements OnInit {
-  constructor( public personaService: PersonaService ) {}
+  constructor(public personaService: PersonaService, 
+              public vacunaService: VacunaService,
+              public ciudadService: CiudadService,
+              public provinciaService: ProvinciaService
+               ) {}
 
   ngOnInit(): void {
     this.getPersonas();
+    this.getVacunas();
+    this.getProvincia();
   }
 
   addPersona(form: NgForm) {
       if (form.value._id) {
-        this.personaService.putPersona(form.value).subscribe((res) => {
+          this.personaService.putPersona(form.value).subscribe((res) => {
           this.resetForm(form);
           this.getPersonas();
+         
         });
+        console.log("if");
       } else {
-        this.personaService.postPersona(form.value).subscribe((res) => {
+          this.personaService.postPersona(form.value).subscribe((res) => {
           this.getPersonas();
           this.resetForm(form);
         });
+        console.log("else"+form.value);
       }
     }
   
-
   getPersonas() {
-    this.personaService.getPersonas().subscribe((res) => {
+      this.personaService.getPersonas().subscribe((res) => {
       this.personaService.personas = res;
     });
   }
+
+  getVacunas() {
+      this.vacunaService.getVacunas().subscribe((res) => {
+      this.vacunaService.vacunas = res;
+    });
+  }
   
+  getProvincia() {
+      this.provinciaService.getProvincias().subscribe((res) => {
+      this.provinciaService.provincias = res;
+      console.log("provincia:"+this.provinciaService.provincias);
+    });
+  }
+  getCiudades(idProvincia:number) {
+      this.ciudadService.getCiudades(idProvincia).subscribe((res) => {
+      this.ciudadService.ciudades = res;
+    });
+  }
   editPersona(persona: Persona) {
     this.personaService.selectedPersona = persona;
   }
@@ -52,11 +83,16 @@ export class PersonaComponent implements OnInit {
     }
   }
 
-  resetForm(form?: NgForm) {
+  resetForm(form: NgForm) {
     if (form) {
-      form.reset();
+      //form.reset();
       this.personaService.selectedPersona = new Persona();
     }
+  }
+  onChageProvincia(valor:any)
+  {
+      console.log("aqui provincia: "+valor);
+      this.getCiudades(valor as number);
   }
 
 }
--- my-app/src/app/services/ciudad.service.ts
+++ my-app/src/app/services/ciudad.service.ts
@@ -7,5 +7,14 @@ import { Ciudad } from '../models/ciudad';
 })
 export class CiudadService {
 
-  constructor() { }
+  ciudades: Ciudad[];
+  readonly URL_API="http://localhost:3000/api/ciudad";
+  constructor(private http:HttpClient) {
+       this.ciudades =[];
+  }
+   
+  getCiudades(idProvincia:number)
+  {
+    return this.http.get<Ciudad[]>(this.URL_API+`/${idProvincia}`);
+  }
 }
